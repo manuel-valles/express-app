9294a51eb3582b1eb6116f057a341457f497b06c
Manuel Valles <manukempo@gmail.com>
2018-02-01 13:37:52 +0000

    Basic Routing - Use nodemon

--- app.js
+++ app.js
@@ -2,6 +2,19 @@
 const express = require('express');
 // Initialize the aplication
 const app = express();
+
+
+// Index Route - Get request
+app.get('/', (req, res)=>{
+	// Send method - send sth to the browser
+	res.send('INDEX');
+});
+
+// About Route
+app.get('/about', (req, res)=>{
+	res.send('ABOUT');
+})
+
 // Port Varibale
 const port = 5000;
 
